ff77750163fa49e4e4f86b87f5e0a0a5e54965f4
Jerry <jaroslawkuliga@gmail.com>
2019-01-20 17:46:08 +0100

    initial settings, add gh pages, styled-components

---
+++ src/view/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { AppContainer } from './App.s';
+
+class App extends Component {
+  render() {
+    return (
+      <AppContainer>
+      	<p>BT controller</p>
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
---
+++ src/view/App.s.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  height: 100vh;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+  background: 
+    linear-gradient(180deg, #3a3a3a, #161616) padding-box,
+    linear-gradient(180deg, #2b2d2c, #2a2c2b) border-box;
+`
