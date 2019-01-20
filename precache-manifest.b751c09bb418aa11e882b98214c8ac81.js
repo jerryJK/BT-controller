d231e34e13d5894313889ae311ea10ca523c1945
Jerry <jaroslawkuliga@gmail.com>
2019-01-20 17:27:43 +0100

    Updates

---
+++ precache-manifest.b751c09bb418aa11e882b98214c8ac81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BT-controller/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed4326a04c89522c1754",
+    "url": "/BT-controller/static/js/runtime~main.ed4326a0.js"
+  },
+  {
+    "revision": "295b5ebe738002b52866",
+    "url": "/BT-controller/static/js/main.295b5ebe.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/BT-controller/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "295b5ebe738002b52866",
+    "url": "/BT-controller/static/css/main.5ddb468f.chunk.css"
+  },
+  {
+    "revision": "461bfeac300dfa571cabb0b20f64a217",
+    "url": "/BT-controller/index.html"
+  }
+];
